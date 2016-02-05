ba07347a087f5e6b7da957a1fabe2db053f8bbcf
Tim Metcalfe <tmetcalfe1@sheffield.ac.uk>
2016-02-05 13:25:58 +0000

    Update main.py
    Added coffee and sugar sections. Yes, I realise that having the same lines repeatedly is ridiculous ha... But it works!

--- main.py
+++ main.py
@@ -1,28 +1,58 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
-tescoUrl = "http://www.tesco.com/groceries/product/browse/default.aspx?N=4293863468&Ne=4294793660&icid=DRI_LHN_Tea&Nao="
+import sys
+sys.stdout = open("C:\\Users\\megaw_000\\Desktop\\super_scraper-master\\outputTesco.txt", "w")
 
-pagesList = ['0', '40', '80', '120', '160', '200', '240', '280']
+tescoUrlTea = "http://www.tesco.com/groceries/product/browse/default.aspx?N=4293863468&Ne=4294793660&Nao="
+pagesListTea = ['0', '20', '40', '60', '80', '100', '120', '140', '160', '180', '200', '220', '240', '260', '280']
+print('TEA:')
 
-# something like for x = 0:len(pagesList):
-    # fullUrl= tescoUrl+pagesList[x]
+for page in pagesListTea:
+    fullUrl = tescoUrlTea+page
+    r = requests.get(fullUrl)
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
 
-fullUrl = tescoUrl+pagesList[0]
-r = requests.get(fullUrl)
-data = r.text
-soup = BeautifulSoup(data, "html.parser")
-
-# Find all elements that exist in product.clearfix class
-productsFrames = soup.find_all("li", class_="product")
-
-with open("tesco.csv", "w") as csvfile:
+    # Find all elements that exist in product.clearfix class
+    productsFrames = soup.find_all("li", class_="product")
 
     for product in productsFrames:
         product_name = product.img["title"].split('-')[0]
         price = product.find(class_="linePrice").string
-        print("{}: {}".format(product_name, price[2:6]))
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow([product_name, price[2:6]])
+        print("{};{}".format(product_name, price[2:6]))
 
-# print(len(productsFrames))
+tescoUrlCoffee = "http://www.tesco.com/groceries/product/browse/default.aspx?N=4294107884&Ne=4294793660&Nao="
+pagesListCoffee = ['0', '20', '40', '60', '80', '100', '120', '140', '160', '180', '200', '220', '240', '260']
+print('COFFEE:')
+
+for page in pagesListCoffee:
+    fullUrl = tescoUrlCoffee+page
+    r = requests.get(fullUrl)
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+
+    # Find all elements that exist in product.clearfix class
+    productsFrames = soup.find_all("li", class_="product")
+
+    for product in productsFrames:
+        product_name = product.img["title"].split('-')[0]
+        price = product.find(class_="linePrice").string
+        print("{};{}".format(product_name, price[2:6]))
+
+tescoUrlSugar = "http://www.tesco.com/groceries/product/browse/default.aspx?N=4294792852&Ne=4294793660&Nao="
+pagesListSugar = ['0', '20', '40', '60']
+print('SUGAR')
+
+for page in pagesListSugar:
+    fullUrl = tescoUrlSugar+page
+    r = requests.get(fullUrl)
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+
+    # Find all elements that exist in product.clearfix class
+    productsFrames = soup.find_all("li", class_="product")
+
+    for product in productsFrames:
+        product_name = product.img["title"].split('-')[0]
+        price = product.find(class_="linePrice").string
+        print("{};{}".format(product_name, price[2:6]))
