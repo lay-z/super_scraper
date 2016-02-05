9f62f707bdd01b91616655a4e115ecbf5501c5f3
megawalrus <tmetcalfe1@sheffield.ac.uk>
2016-02-05 10:54:00 +0000

    Update main.py
    - changed URL to tea section rather than a search for tea
    - removed item variable - redundant now that we're not using the search URL
    - added pageList - list containing URL endings for the different pages
    - started for loop on line 8, to loop over the different pages and scrape each URL in turn... (probably not correct, though!)
    - now scraping for linePrice, rather than linePriceAbbr (Flavia said the former is all that's really needed)
    - adjusted string parsing to account for linePrice change

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-tescoSearchUrl = "http://www.tesco.com/groceries/product/search/default.aspx?searchBox=tea&newSort=true&search="
-item = "tea"
-r = requests.get(tescoSearchUrl+item)
+tescoUrl = "http://www.tesco.com/groceries/product/browse/default.aspx?N=4293863468&Ne=4294793660&icid=DRI_LHN_Tea&Nao="
+
+pagesList = ['0', '40', '80', '120', '160', '200', '240', '280']
+
+# something like for x = 0:len(pagesList):
+    # fullUrl= tescoUrl+pagesList[x]
+
+fullUrl = tescoUrl+pagesList[0]
+r = requests.get(fullUrl)
 data = r.text
 soup = BeautifulSoup(data, "html.parser")
 
-# find all elements that exist in product.clearfix class
+# Find all elements that exist in product.clearfix class
 productsFrames = soup.find_all("li", class_="product")
 
-with open("output.csv", "w") as csvfile:
+with open("tesco.csv", "w") as csvfile:
 
     for product in productsFrames:
-        print(product)
         product_name = product.img["title"].split('-')[0]
-        price = product.find(class_="linePriceAbbr").string
-        print("{}: {}".format(product_name,price[2:-1]))
+        price = product.find(class_="linePrice").string
+        print("{}: {}".format(product_name, price[2:6]))
         writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow([product_name, price[2:-1]])
-
+        writer.writerow([product_name, price[2:6]])
 
 # print(len(productsFrames))
