b9e6442b02d9217a8b0448f5cddaeb8d6d05346f
Priyav <priyavrocks@gmail.com>
2016-02-07 11:34:40 +0000

    refactored code

--- main.py
+++ main.py
@@ -1,58 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
 import sys
-sys.stdout = open("C:\\Users\\megaw_000\\Desktop\\super_scraper-master\\outputTesco.txt", "w")
+sys.stdout = open("outputTesco.txt", "w")
+
+
+
+def tescoScraper(url, end):
+    increment = 20
+    start = 0
+    pagesList = range(start, end+increment, increment)  # range produces a list start (inclusive) - end (exclusive) with increment seperation
+
+    for page in pagesList:
+        fullUrl = url+str(page)  # have to convert int to string to concatinate the two together
+        r = requests.get(fullUrl)
+        data = r.text
+        soup = BeautifulSoup(data, "html.parser")
+
+        # Find all elements that exist in product.clearfix class
+        productsFrames = soup.find_all("li", class_="product")
+
+        for product in productsFrames:
+            product_name = product.img["title"].split('-')[0]
+            price = product.find(class_="linePrice")
+            if price is not None:
+                price = price.string
+            else:
+                price = "NA"  # not available currently
+            print("{};{}".format(product_name[:-1], price[2:6]))  # added the '-1' to remove space at the end of product description
+
+    print("")  # spacing betwen each scrape
 
 tescoUrlTea = "http://www.tesco.com/groceries/product/browse/default.aspx?N=4293863468&Ne=4294793660&Nao="
-pagesListTea = ['0', '20', '40', '60', '80', '100', '120', '140', '160', '180', '200', '220', '240', '260', '280']
 print('TEA:')
-
-for page in pagesListTea:
-    fullUrl = tescoUrlTea+page
-    r = requests.get(fullUrl)
-    data = r.text
-    soup = BeautifulSoup(data, "html.parser")
-
-    # Find all elements that exist in product.clearfix class
-    productsFrames = soup.find_all("li", class_="product")
-
-    for product in productsFrames:
-        product_name = product.img["title"].split('-')[0]
-        price = product.find(class_="linePrice").string
-        print("{};{}".format(product_name, price[2:6]))
+tescoScraper(tescoUrlTea, 280)
 
 tescoUrlCoffee = "http://www.tesco.com/groceries/product/browse/default.aspx?N=4294107884&Ne=4294793660&Nao="
-pagesListCoffee = ['0', '20', '40', '60', '80', '100', '120', '140', '160', '180', '200', '220', '240', '260']
 print('COFFEE:')
-
-for page in pagesListCoffee:
-    fullUrl = tescoUrlCoffee+page
-    r = requests.get(fullUrl)
-    data = r.text
-    soup = BeautifulSoup(data, "html.parser")
-
-    # Find all elements that exist in product.clearfix class
-    productsFrames = soup.find_all("li", class_="product")
-
-    for product in productsFrames:
-        product_name = product.img["title"].split('-')[0]
-        price = product.find(class_="linePrice").string
-        print("{};{}".format(product_name, price[2:6]))
+tescoScraper(tescoUrlCoffee, 260)
 
 tescoUrlSugar = "http://www.tesco.com/groceries/product/browse/default.aspx?N=4294792852&Ne=4294793660&Nao="
-pagesListSugar = ['0', '20', '40', '60']
 print('SUGAR')
+tescoScraper(tescoUrlSugar, 60)
 
-for page in pagesListSugar:
-    fullUrl = tescoUrlSugar+page
-    r = requests.get(fullUrl)
-    data = r.text
-    soup = BeautifulSoup(data, "html.parser")
 
-    # Find all elements that exist in product.clearfix class
-    productsFrames = soup.find_all("li", class_="product")
-
-    for product in productsFrames:
-        product_name = product.img["title"].split('-')[0]
-        price = product.find(class_="linePrice").string
-        print("{};{}".format(product_name, price[2:6]))
